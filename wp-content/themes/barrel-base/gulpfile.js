1f04a3e0cf98605765a0401cf6172bcb0ed444b6
Wes Turner <wturnerharris@gmail.com>
2016-05-11 08:47:24 -0400

    chore: add contributing info, adjust gulp
    - Add contributing markdown
    - Add gitlab base ci config
    - Update config for code styling and hinting
    - Add vendor script task

--- wp-content/themes/barrel-base/gulpfile.js
+++ wp-content/themes/barrel-base/gulpfile.js
@@ -1,28 +1,47 @@
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
+var gulp = require( "gulp" );
+var livereload = require( "gulp-livereload" );
+var path = require( "path" );
 
-require('./tasks/sass');
-require('./tasks/jscs');
-require('./tasks/jshint');
-require('./tasks/browserify');
+require( "./tasks/sass" );
+require( "./tasks/jscs" );
+require( "./tasks/jshint" );
+require( "./tasks/browserify" );
+require( "./tasks/vendors" );
 
 /** Defines the "build" task for Gulp. */
-gulp.task('build', ['sass', 'jscs', 'jshint', 'browserify']);
+gulp.task( "build", [ "vendors", "sass", "browserify" ]);
+
+/** Defines the "validate" task for Gulp. */
+gulp.task( "validate", [ "jshint", "jscs" ]);
 
 /** Defines the "dev" task for Gulp. */
-gulp.task('dev', ['sass', 'watchify'], function() {
+gulp.task( "dev", [ "vendors", "sass", "watchify" ], function() {
   livereload.listen();
 
   // Watch stylesheets
-  gulp.watch(['./**/*.scss'], ['sass']);
+  gulp.watch([ "./**/*.scss" ], [ "sass" ]);
 
-  // Watchify handles the scripts
+  // Watch handles the scripts
+  gulp.watch([ "./src/js/**/*.js", "./tasks/*.js", "./gulpfile.js" ], function( event ) {
+    if ( event.type == "changed" ) {
+      if ( path.extname( event.path ) == ".js" ) {
+        var jscs = require( "gulp-jscs" );
+        var jshint = require( "gulp-jshint" );
+
+        gulp.src([ event.path ])
+        .pipe( jshint() )
+        .pipe( jshint.reporter( "jshint-stylish" ) )
+        .pipe( jscs({ fix: true }) )
+        .pipe( jscs.reporter( "jscs-stylish" ) );
+      }
+    }
+  });
 
   // When compile tasks finish, trigger livereload
-  gulp.watch(['./assets/**/*.css', './assets/**/*.js'], function(event) {
-    livereload.changed(event.path);
+  gulp.watch([ "./assets/**/*.css", "./assets/**/*.js" ], function( event ) {
+    livereload.changed( event.path );
   });
 });
 
 /** Default is "dev". */
-gulp.task('default', ['dev']);
+gulp.task( "default", [ "dev" ]);
