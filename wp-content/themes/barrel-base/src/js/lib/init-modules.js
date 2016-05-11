92cd49b46aeaa58fd014fd324785ab775725ec7b
Wes Turner <wturnerharris@gmail.com>
2016-05-11 08:49:49 -0400

    style: update code styling
    - Add missing node packages
    - Process code style routine

--- wp-content/themes/barrel-base/src/js/lib/init-modules.js
+++ wp-content/themes/barrel-base/src/js/lib/init-modules.js
@@ -1,31 +1,31 @@
-var $ = require('jquery');
+var $ = require( "jquery" );
 
 /**
  * Finds all elements with a "data-module-init" attribute
  * and calls the corresponding script
  */
 function initializeModules() {
-  var modules = document.querySelectorAll('[data-module-init]');
+  var modules = document.querySelectorAll( "[data-module-init]" );
 
-  for (var i = 0; i < modules.length; i++) {
-    var el = modules[i];
-    var $el = $(el);
-    var name = el.getAttribute('data-module-init');
+  for ( var i = 0; i < modules.length; i++ ) {
+    var el = modules[ i ];
+    var $el = $( el );
+    var name = el.getAttribute( "data-module-init" );
 
     // Find the module script
     try {
-      var Module = require('modules/'+name);
-    } catch(e) {
-      console.log(e.toString());
+      var Module = require( "modules/" + name );
+    } catch ( e ) {
+      console.log( e.toString() );
       var Module = false;
     }
 
     // Initialize the module with the calling element
-    if(typeof Module === 'function' && !$el.data('module')) {
-      var mod = new Module(el);
+    if ( typeof Module === "function" && !$el.data( "module" ) ) {
+      var mod = new Module( el );
 
       // Save module instance to jQuery data
-      $(el).data('module', mod);
+      $( el ).data( "module", mod );
     }
   }
 }
--- wp-content/themes/barrel-base/tasks/jshint.js
+++ wp-content/themes/barrel-base/tasks/jshint.js
@@ -1,8 +1,9 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
- 
-gulp.task('jshint', function() {
-  return gulp.src(['src/js/**/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter());
+var gulp = require( "gulp" );
+var jshint = require( "gulp-jshint" );
+
+gulp.task( "jshint", function() {
+  return gulp.src([ "gulpfile.js", "tasks/*.js", "src/js/**/*.js" ])
+    .pipe( jshint() )
+    .pipe( jshint.reporter( "jshint-stylish" ) )
+    .pipe( jshint.reporter( "fail" ) );
 });
