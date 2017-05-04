7480674950abba9344e9f12a6774f33d872201b8
Wes Turner <wturnerharris@gmail.com>
2017-05-04 08:55:24 -0400

    Remove searchwp hook from base theme class

--- wp-content/themes/barrel-base/lib/class-theme-init.php
+++ wp-content/themes/barrel-base/lib/class-theme-init.php
@@ -34,8 +34,6 @@ class Base_Theme extends BB_Theme {
 		remove_filter( 'acf_the_content', array( &$this, 'wpautop') );
 		add_filter( 'default_page_template_title', array(&$this, 'rename_default_template' ) );
 
-		add_filter( 'searchwp_custom_fields', array(&$this, 'index_acf_fields' ) );
-
 		$this->add_options_page();
 		$this->register_image_sizes();
 	}
@@ -424,23 +422,6 @@ class Base_Theme extends BB_Theme {
 		return '...';
 	}
 
-	public function index_acf_fields( $custom_field_value, $custom_field_name, $the_post ) {
-		$post_id = $the_post->ID;
-		$has_redirect = get_post_meta($post_id, 'redirect_event_page', true);
-		$redirect_type = get_post_meta($post_id, 'event_redirect_link_type', true);
-		$link_id = (int) get_post_meta($post_id, 'event_page_link', true);
-
-		// index the custom field value if this is not an event, or if it doesn't have an event_page_link field
-		if ( $custom_field_name !== 'event_page_link' || !$has_redirect || $redirect_type !== 'internal' || !$link_id ) {
-			return $custom_field_value;
-		}
-
-		// index the title and content of the linked post
-		$linked_post = get_post( $link_id );
-        $content_to_index = $linked_post->post_title . ' ' . $linked_post->post_content;
-
-		return $content_to_index;
-	}
 }
 
 new Base_Theme();
