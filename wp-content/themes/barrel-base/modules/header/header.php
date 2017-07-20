78b6c365b8787a6e06de8a36d351f6f6840a81ee
Ben Godfrey <ben.godfrey@barrelny.com>
2017-07-20 12:05:25 -0400

    Updates header with site name, description, and social links

--- wp-content/themes/barrel-base/modules/header/header.php
+++ wp-content/themes/barrel-base/modules/header/header.php
@@ -1,3 +1,39 @@
 <header class="header" data-module-init="header">
-  <?php wp_nav_menu(); ?>
+
+  <div>
+    <?php if ( is_front_page() ) : ?>
+      <h1>
+        <?php bloginfo( 'name' ); ?>
+      </h1>
+      <h2><?php bloginfo( 'description' ); ?></h2>
+    <?php else : ?>
+      <p>
+        <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+          <?php bloginfo( 'name' ); ?>
+        </a>
+      </p>
+      <p><?php bloginfo( 'description' ); ?></p>
+    <?php endif; ?>
+  </div>
+
+  <?php 
+    wp_nav_menu( array(
+      'container' => 'nav'
+    ) );
+  ?>
+
+  <?php if( have_rows('social_media_links', 'options') ): ?>
+    <nav>
+      <ul>
+        <?php while( have_rows('social_media_links', 'options') ): the_row(); ?>
+          <li>
+            <a href="<?php echo the_sub_field('url'); ?>" target="_blank">
+              <?php echo the_sub_field('label'); ?>
+            </a>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+    </nav>
+  <?php endif; ?>
+
 </header>
