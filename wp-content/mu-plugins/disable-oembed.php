5a0fa246a988b8a5b714306018c9271d8b5ca093
Wes Turner <wturnerharris@gmail.com>
2017-08-24 15:06:56 -0400

    Add commonly used scripts to must-use plugins
    * Add disable xml-rpc api
    * Add disable oembed

--- wp-content/mu-plugins/disable-oembed.php
+++ wp-content/mu-plugins/disable-oembed.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Disable XML RPC
+Plugin Name: Disable Oembed Scripts
 Plugin URI: https://code.barrelny.com/
 Description: Common modules for any WordPress website.
 Version: 0.1
@@ -8,4 +8,6 @@ Author: Barrel
 Author URI: https://www.barrelny.com/
 */
 
-add_filter( 'xmlrpc_enabled', '__return_false' );
+add_action( 'wp_footer', function () {
+	wp_deregister_script( 'wp-embed' );
+} );
