d5f1cf90b2bfc95aefb4ef981a4ac375c4a8e484
Wes Turner <wturnerharris@gmail.com>
2018-03-13 16:00:05 -0400

    Update CPTUI plugin

--- wp-content/plugins/custom-post-type-ui/inc/utility.php
+++ wp-content/plugins/custom-post-type-ui/inc/utility.php
@@ -24,8 +24,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return array Amended array of links.
  */
 function cptui_edit_plugin_list_links( $links ) {
-	// We shouldn't encourage editing our plugin directly.
-	unset( $links['edit'] );
+
+	if ( is_array( $links ) && isset( $links['edit'] ) ) {
+		// We shouldn't encourage editing our plugin directly.
+		unset( $links['edit'] );
+	}
 
 	// Add our custom links to the returned array value.
 	return array_merge( array(
@@ -591,7 +594,8 @@ function cptui_import_success_admin_notice() {
  */
 function cptui_import_fail_admin_notice() {
 	echo cptui_admin_notices_helper(
-		esc_html__( 'Invalid data provided', 'custom-post-type-ui' )
+		esc_html__( 'Invalid data provided', 'custom-post-type-ui' ),
+		false
 	);
 }
 
