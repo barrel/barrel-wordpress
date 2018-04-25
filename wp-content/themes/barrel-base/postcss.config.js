4e286e82db384c137e5b5976460998d01f907418
Kevin Nguyen <minhkhoi.nguyen@solislab.com>
2018-04-25 14:22:53 +0700

    Add postcss-fontpath package

--- wp-content/themes/barrel-base/postcss.config.js
+++ wp-content/themes/barrel-base/postcss.config.js
@@ -3,7 +3,15 @@ const config = {
   plugins: [
     require('./tasks/postcss-module-import'),
     require('postcss-sassy-mixins'),
-
+    require('postcss-fontpath')({
+      format: [
+        { type: 'embedded-opentype', ext: 'eot' },
+        { type: 'woff', ext: 'woff' },
+        { type: 'truetype', ext: 'ttf' },
+        { type: 'svg', ext: 'svg'}
+      ],
+      checkFiles: true
+    }),
     require('autoprefixer')({
       browsers: [
         'last 3 versions',
