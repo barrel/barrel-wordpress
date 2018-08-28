4b56c839629f6aa33cba2b3fbfdf3828adfd06c7
Jon Hendershot <jon.hendershot@barrelny.com>
2018-08-28 21:30:50 +0000

    Update YOAST

--- wp-content/plugins/wordpress-seo/inc/sitemaps/class-sitemaps-router.php
+++ wp-content/plugins/wordpress-seo/inc/sitemaps/class-sitemaps-router.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * WPSEO plugin file.
+ *
  * @package WPSEO\XML_Sitemaps
  */
 
@@ -63,10 +65,16 @@ class WPSEO_Sitemaps_Router {
 			$current_url = 'https://';
 		}
 
-		$current_url .= sanitize_text_field( $_SERVER['SERVER_NAME'] );
+		$domain = $_SERVER['SERVER_NAME'];
+		if ( ! empty( $_SERVER['HTTP_HOST'] ) ) {
+			$domain = $_SERVER['HTTP_HOST'];
+		}
+
+		$current_url .= sanitize_text_field( $domain );
 		$current_url .= sanitize_text_field( $_SERVER['REQUEST_URI'] );
 
 		if ( home_url( '/sitemap.xml' ) === $current_url && $wp_query->is_404 ) {
+			header( 'X-Redirect-By: Yoast SEO' );
 			wp_redirect( home_url( '/sitemap_index.xml' ), 301 );
 			exit;
 		}
--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit503e08848fb9da26d79854e9fc1ab0e6::getLoader();
+return ComposerAutoloaderInitfbe5b803497ba919f3726be310778fcd::getLoader();
