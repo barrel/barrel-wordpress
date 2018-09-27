98874b91f4157bf6e173c0b2713fb21f86698827
Jon Hendershot <jon.hendershot@barrelny.com>
2018-09-27 14:12:47 -0400

    Remove extra linebreaks

--- private/scripts/test_json.sh
+++ private/scripts/test_json.sh
@@ -10,22 +10,23 @@ THEME_NAME="barrel-base"
 INVALID_FILES=""
 COUNT=0
 
+echo "Validating json files in the ${THEME_NAME} theme"
 while read -r line; do
     php -r "if ( ! json_decode(file_get_contents('$line')) ) { exit(1); }"
     if [ $? -ne 0 ]; then
         if [ $COUNT -eq 0 ]; then
-            INVALID_FILES+="INVALID JSON FILES:\n"
+            INVALID_FILES+="INVALID JSON FILES:"
         fi
-        INVALID_FILES+="${line}\n"
+        INVALID_FILES+="${line}"
         COUNT=$(($COUNT+1))
     fi
 done <<< "$(rg wp-content/themes/${THEME_NAME} --files --type json)"
 
 if [ -z "$INVALID_FILES" ]; then
-    echo "\nAll JSON files look ok. Moving on ...\n"
+    echo "All JSON files look ok. Moving on ..."
     exit
 else
-    echo "\nJSON Syntax errors were found in the ${THEME_NAME} theme. Please check the snytax of the following files and fix any errors.\n"
+    echo "JSON Syntax errors were found in the ${THEME_NAME} theme. Please check the snytax of the following files and fix any errors."
     echo $INVALID_FILES
     exit 1;
 fi
