e5e779292a0c5b63a3fab3f5c04634f9e6abc9d8
Wes Turner <wturnerharris@gmail.com>
2018-10-10 19:26:53 -0400

    Refactor multidev auto-deploy to work with multiple branch names/types

--- private/scripts/multidev_deploy.sh
+++ private/scripts/multidev_deploy.sh
@@ -1,44 +1,89 @@
 #!/bin/bash
 
-PREFIX="feature/"
-ENV=$(echo ${CI_COMMIT_REF_NAME:${#PREFIX}:11} | tr '[:upper:]' '[:lower:]')
-MATCH=$(terminus multidev:list --format=list --fields=id $PANTHEON_SITE_ID | grep $ENV)
+####################################################################
+## This script will deploy to a pantheon site's multidev environment 
+## and associated branch.
+####################################################################
 
-echo "Checking if ENV '$ENV' exists ..."
-if [ "$MATCH" != "$ENV" ]
-then 
-    echo "Multidev not found. Creating $ENV from develop ..."
-    terminus multidev:create $PANTHEON_SITE_ID.develop $ENV
-    terminus remote:wp $PANTHEON_SITE_ID.$ENV -- theme activate $THEME_NAME
-	printf "\ndone\n"
+# Variables
+# Terminal colors
+DEFAULT=$(tput setaf 7 -T xterm)
+RED=$(tput setaf 1 -T xterm)
+GREEN=$(tput setaf 2 -T xterm)
+YELLOW=$(tput setaf 3 -T xterm)
+BLUE=$(tput setaf 4 -T xterm)
+
+# CI_COMMIT_REF_NAME, defined by GitLab CI or by user
+# PANTHEON_SITE_ID, defined in environment variables or by user
+# ENV, defined automatically
+
+if [ -z ${CI_COMMIT_REF_NAME+x} ]; then
+    echo "${YELLOW}Hmm... Looks like this is not being run in GitLab CI, what's the original branch name?${DEFAULT}"
+    read CI_COMMIT_REF_NAME
+fi
+if [ -z ${PANTHEON_SITE_ID+x} ]; then
+    echo "${YELLOW}Hmm... Something is missing. What is the Pantheon Site Name?${DEFAULT}"
+    read PANTHEON_SITE_ID
+fi
+DONE="\n${GREEN}done${DEFAULT}\n"
+TARGET=$(echo $CI_COMMIT_REF_NAME | cut -d'/' -f2)
+ENV=$(echo ${TARGET:0:11} | tr '[:upper:]' '[:lower:]') 
+MATCH=$(terminus multidev:list --format=list --fields=id $PANTHEON_SITE_ID | grep $ENV)
+LIVE_TAG=$(git ls-remote --tags pantheon | grep "pantheon_live_")
+LIVE_EXISTS="$?"
+TEST_TAG=$(git ls-remote --tags pantheon | grep "pantheon_test_")
+TEST_EXISTS="$?"
+
+echo "${YELLOW}Checking which database should be used...${DEFAULT}"
+
+if [[ "$LIVE_EXISTS" -eq 0 ]]; then
+    DATA_ENVIRONMENT="live"
+elif [[ "$TEST_EXISTS" -eq 0 ]]; then
+    DATA_ENVIRONMENT="test"
+else
+    DATA_ENVIRONMENT="dev"
 fi
 
-echo "Setting Pantheon '$ENV' to git mode ..."
+echo "${YELLOW}Looks like the most recent data can be found in the <$DATA_ENVIRONMENT> environment.${DEFAULT}"
+echo "${YELLOW}Checking if ENV '$ENV' exists..."
+if [[ "$MATCH" -ne 0 ]]
+then 
+    echo "${YELLOW}Multidev not found. Creating $ENV from the <$DATA_ENVIRONMENT> environment...${DEFAULT}"
+    terminus multidev:create $PANTHEON_SITE_ID.$DATA_ENVIRONMENT $ENV
+    terminus remote:wp $PANTHEON_SITE_ID.$ENV -- theme activate $THEME_NAME
+    echo -ne $DONE
+else
+    echo "${YELLOW}Looks like the $ENV environment exists.. cloning data from <$DATA_ENVIRONMENT>...${DEFAULT}"
+    terminus env:clone-content $PANTHEON_SITE_ID.$DATA_ENVIRONMENT $ENV
+    echo -ne $DONE
+fi
+
+echo "${YELLOW}Setting Pantheon '$ENV' to git mode...${DEFAULT}"
 terminus connection:set $PANTHEON_SITE_ID.$ENV git
-printf "\ndone\n"
+echo -ne $DONE
 
-echo "Pushing to '$ENV' on Pantheon ..."
+echo "${YELLOW}Pushing to '$ENV' on Pantheon...${DEFAULT}"
 git push -f pantheon HEAD:$ENV
-printf "\ndone\n"
+echo -ne $DONE
 
-echo "Installing theme build tools ..."
+echo "${YELLOW}Installing theme build tools...${DEFAULT}"
 cd ./wp-content/themes/$THEME_NAME && npm i
-printf "\ndone\n"
+echo -ne $DONE
 
-echo "Building theme ..."
+echo "${YELLOW}Building theme...${DEFAULT}"
 npm run build
-printf "\ndone\n"
+echo -ne $DONE
 
 # Just checking if there are changes, surely there's more to consider
 CHANGED=$(git status --porcelain)
 if [ -n "$CHANGED" ]
 then
-    echo "Changes detected. Committing ..."
+    echo "${YELLOW}Changes detected. Committing...${DEFAULT}"
     git commit -am "Process scripts and styles"
-    echo "Pushing ..."
+    echo "${YELLOW}Pushing...${DEFAULT}"
     git push pantheon HEAD:$ENV --verbose
-	printf "\ndone\n"
+    echo -ne $DONE
 else
-	echo "No changes ..."
-	printf "\ndone\n"
+	echo "${YELLOW}No changes...${DEFAULT}"
+    echo -ne $DONE
 fi
