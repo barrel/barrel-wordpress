7916c9ff39884bbfa6b18203fb510069346874a4
Wesley Turner-Harris <wturnerharris@gmail.com>
2018-10-11 01:26:34 -0400

    Clean up multidev deploy script

--- private/scripts/multidev_deploy.sh
+++ private/scripts/multidev_deploy.sh
@@ -20,72 +20,117 @@ YELLOW=$(tput setaf 3 -T xterm)
 BLUE=$(tput setaf 4 -T xterm)
 
 if [ -z ${CI_COMMIT_REF_NAME+x} ]; then
-    echo "${YELLOW}Hmm... Looks like this is not being run in GitLab CI, what's the original branch name?${DEFAULT}"
+    echo -e "${BLUE}Hmm... Looks like this is not being run in GitLab CI. What's the original branch name?${DEFAULT}"
     read CI_COMMIT_REF_NAME
 fi
+
 if [ -z ${PANTHEON_SITE_ID+x} ]; then
-    echo "${YELLOW}Hmm... Something is missing. What is the Pantheon Site Name?${DEFAULT}"
+    echo -e "${BLUE}Hmm... Something is missing. What is the Pantheon Site Name?${DEFAULT}"
     read PANTHEON_SITE_ID
 fi
-DONE="\n${GREEN}done${DEFAULT}\n"
+
+if [ -z ${THEME_NAME+x} ]; then
+    echo -e "${BLUE}Hmm... Something is missing. What is the Theme Name?${DEFAULT}"
+    read THEME_NAME
+fi
+
+DONE="${GREEN}...done${DEFAULT}\n"
 TARGET=$(echo $CI_COMMIT_REF_NAME | cut -d'/' -f2)
 ENV=$(echo ${TARGET:0:11} | tr '[:upper:]' '[:lower:]') 
+
+echo -e "${YELLOW}Checking which database should be used...${DEFAULT}"
+
 MATCH=$(terminus multidev:list --format=list --fields=id $PANTHEON_SITE_ID | grep $ENV)
+MATCH_EXISTS="$?"
+
 LIVE_TAG=$(git ls-remote --tags pantheon | grep "pantheon_live_")
 LIVE_EXISTS="$?"
+
 TEST_TAG=$(git ls-remote --tags pantheon | grep "pantheon_test_")
 TEST_EXISTS="$?"
 
-echo "${YELLOW}Checking which database should be used...${DEFAULT}"
-
-if [[ "$LIVE_EXISTS" -eq 0 ]]; then
+if [ "$LIVE_EXISTS" -eq "0" ]; then
     DATA_ENVIRONMENT="live"
-elif [[ "$TEST_EXISTS" -eq 0 ]]; then
+elif [ "$TEST_EXISTS" -eq "0" ]; then
     DATA_ENVIRONMENT="test"
 else
     DATA_ENVIRONMENT="dev"
 fi
 
-echo "${YELLOW}Looks like the most recent data can be found in the <$DATA_ENVIRONMENT> environment.${DEFAULT}"
-echo "${YELLOW}Checking if ENV '$ENV' exists..."
-if [[ "$MATCH" -ne 0 ]]
+echo -e "Looks like the most recent data can be found in the <$DATA_ENVIRONMENT> environment.\n"
+echo -e "${YELLOW}Checking if ENV '$ENV' exists...${DEFAULT}"
+
+if [[ "$MATCH_EXISTS" -ne 0 ]]
 then 
-    echo "${YELLOW}Multidev not found. Creating $ENV from the <$DATA_ENVIRONMENT> environment...${DEFAULT}"
+    echo -e "Multidev not found.\n"
+    echo -e "${YELLOW}Creating $ENV from the <$DATA_ENVIRONMENT> environment...${DEFAULT}"
     terminus multidev:create $PANTHEON_SITE_ID.$DATA_ENVIRONMENT $ENV
+    if [[ "$?" -ne 0 ]]; then
+        exit 1
+    fi
+    echo -ne $DONE
+
+    echo -e "${YELLOW}Activating the theme...${DEFAULT}"
     terminus remote:wp $PANTHEON_SITE_ID.$ENV -- theme activate $THEME_NAME
-    echo -n $DONE
+    if [[ "$?" -ne 0 ]]; then
+        exit 1
+    fi
+    echo -ne $DONE
 else
-    echo "${YELLOW}Looks like the $ENV environment exists.. cloning data from <$DATA_ENVIRONMENT>...${DEFAULT}"
-    terminus env:clone-content $PANTHEON_SITE_ID.$DATA_ENVIRONMENT $ENV
-    echo -n $DONE
+    echo -e "Looks like the $ENV environment exists.\n"
+    echo -e "${YELLOW}Cloning data from <$DATA_ENVIRONMENT>...${DEFAULT}"
+    terminus env:clone-content $PANTHEON_SITE_ID.$DATA_ENVIRONMENT $ENV --yes
+    if [[ "$?" -ne 0 ]]; then
+        exit 1
+    fi
+    echo -ne $DONE
 fi
 
-echo "${YELLOW}Setting Pantheon '$ENV' to git mode...${DEFAULT}"
+echo -e "${YELLOW}Setting Pantheon '$ENV' to git mode...${DEFAULT}"
 terminus connection:set $PANTHEON_SITE_ID.$ENV git
-echo -n $DONE
+if [[ "$?" -ne 0 ]]; then
+    exit 1
+fi
+echo -ne $DONE
 
-echo "${YELLOW}Pushing to '$ENV' on Pantheon...${DEFAULT}"
+echo -e "${YELLOW}Pushing to '$ENV' on Pantheon...${DEFAULT}"
 git push -f pantheon HEAD:$ENV
-echo -n $DONE
+if [[ "$?" -ne 0 ]]; then
+    exit 1
+fi
+echo -ne $DONE
 
-echo "${YELLOW}Installing theme build tools...${DEFAULT}"
+echo -e "${YELLOW}Installing theme build tools...${DEFAULT}"
 cd ./wp-content/themes/$THEME_NAME && npm i
-echo -n $DONE
+if [[ "$?" -ne 0 ]]; then
+    exit 1
+fi
+echo -ne $DONE
 
-echo "${YELLOW}Building theme...${DEFAULT}"
+echo -e "${YELLOW}Building theme...${DEFAULT}"
 npm run build
-echo -n $DONE
+if [[ "$?" -ne 0 ]]; then
+    exit 1
+fi
+echo -ne $DONE
 
 # Just checking if there are changes, surely there's more to consider
 CHANGED=$(git status --porcelain)
 if [ -n "$CHANGED" ]
 then
-    echo "${YELLOW}Changes detected. Committing...${DEFAULT}"
+    echo -e "${YELLOW}Changes detected. Committing...${DEFAULT}"
     git commit -am "Process scripts and styles"
-    echo "${YELLOW}Pushing...${DEFAULT}"
+    if [[ "$?" -ne 0 ]]; then
+        exit 1
+    fi
+
+    echo -e "${YELLOW}Pushing...${DEFAULT}"
     git push pantheon HEAD:$ENV --verbose
-    echo -n $DONE
+    if [[ "$?" -ne 0 ]]; then
+        exit 1
+    fi
+    echo -ne $DONE
 else
 	echo "${YELLOW}No changes...${DEFAULT}"
-    echo -n $DONE
+    echo -ne $DONE
 fi
