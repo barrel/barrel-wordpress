6915208d0615e3b4789be5c246ffe7256a6a0529
Wes Turner <wturnerharris@gmail.com>
2019-03-28 18:07:45 -0400

    Update import git remote script

--- private/scripts/import_git_remote.sh
+++ private/scripts/import_git_remote.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "Verifying git user details ..."
+source ./private/scripts/colors.sh
+
+echo "${YELLOW}Verifying git user details...${DEFAULT}"
 git config --global user.email
 git config --global user.name
-printf "\ndone\n"
+printf "\n%s\n" "$DONE"
 
-echo "Authorizing with Pantheon ..."
+echo "${YELLOW}Authorizing with Pantheon...${DEFAULT}"
 terminus auth:login --machine-token=$TERMINUS_TOKEN
 
 # Pantheon git remote
@@ -13,27 +15,25 @@ REMOTE=$(terminus connection:info --format=list --fields=git_url $PANTHEON_SITE_
 
 if [ $? -gt 0 ]
 then
-	echo "There was a problem authorizing with Pantheon."
+	echo "${RED}There was a problem authorizing with Pantheon.${DEFAULT}"
 	exit 1
 fi
 
-printf "\ndone\n"
+echo $DONE
 
-echo "Checking to make sure the Pantheon git remote exists ..."
+echo "${YELLOW}Checking to make sure the Pantheon git remote exists...${DEFAULT}"
 git remote | grep pantheon
 if test $? != 0;
 then
-	echo "Didn't find the Pantheon git remote. Adding it now ..."
+	echo "${YELLOW}Didn't find the Pantheon git remote. Adding it now...${DEFAULT}"
 	git remote add pantheon $REMOTE
 
 	if [ $? -gt 0 ]
 	then
-		echo "There was a problem adding the Pantheon git remote."
+		echo "${RED}There was a problem adding the Pantheon git remote.${DEFAULT}"
 		exit 1
 	fi
 else
-	echo "Found Pantheon git remote ..."
+	echo "${YELLOW}Found Pantheon git remote...${DEFAULT}"
 fi
-
-
-printf "\ndone\n"
+echo $DONE
