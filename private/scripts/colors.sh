399def33774f7d8e63bc4021a7eed8bf1891e4a5
Wes Turner <wturnerharris@gmail.com>
2019-03-30 20:05:19 -0400

    Update colors script

--- private/scripts/colors.sh
+++ private/scripts/colors.sh
@@ -7,6 +7,15 @@ GREEN=$(tput setaf 2 -T xterm)
 YELLOW=$(tput setaf 3 -T xterm)
 BLUE=$(tput setaf 4 -T xterm)
 BOLD=$(tput bold -T xterm)
-NORMAL=$(tput sgr0 -T xterm)
+RESET=$(tput sgr0 -T xterm)
+DIM=$(tput dim)
+# begin underline mode
+SMUL=$(tput smul)
+# exit underline mode
+RMUL=$(tput rmul)
+# up x lines
+U1=$(tput cuu 1)
+#EOL
+EL=$(tput el)
 OK="${GREEN}ok${DEFAULT}"
 DONE="${GREEN}done${DEFAULT}"
