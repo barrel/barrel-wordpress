1d347bc3d0f70a6e3efd9f7845f6da4e09d864cc
Wes Turner <wturnerharris@gmail.com>
2019-07-30 17:37:24 -0400

    Update redirection plugin

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.2.3' );
-define( 'REDIRECTION_BUILD', '832f7155834bcdc64325009360c6273b' );
-define( 'REDIRECTION_MIN_WP', '4.5' );
+define( 'REDIRECTION_VERSION', '4.3.1' );
+define( 'REDIRECTION_BUILD', '76c2e333a0fb0e417d3c1937d0c4045c' );
+define( 'REDIRECTION_MIN_WP', '4.6' );
